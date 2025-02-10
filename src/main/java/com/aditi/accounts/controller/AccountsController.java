eacf0fa000bbd1cf4b7875617e1fd421b84e161f
Aditi0389 <aditi03learning@gmail.com>
2025-02-10 23:44:18 +0530

    New project added

---
+++ src/main/java/com/aditi/accounts/controller/AccountsController.java
@@ -0,0 +1,29 @@
+package com.aditi.accounts.controller;
+
+import com.aditi.accounts.constants.AccountsConstants;
+import com.aditi.accounts.dto.CustomerDto;
+import com.aditi.accounts.dto.ErrorResponseDto;
+import com.aditi.accounts.dto.ResponseDto;
+import com.aditi.accounts.service.IAccountsService;               
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+  
+@RestController
+@RequestMapping(path="/api", produces = {MediaType.APPLICATION_JSON_VALUE})
+@AllArgsConstructor
+public class AccountsController {
+
+    private IAccountsService iAccountsService;
+
+    @PostMapping("/create")
+    public ResponseEntity<ResponseDto> createAccount(@RequestBody CustomerDto customerDto) {
+        iAccountsService.createAccount(customerDto);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(new ResponseDto(AccountsConstants.STATUS_201, AccountsConstants.MESSAGE_201));
+    }
+}
